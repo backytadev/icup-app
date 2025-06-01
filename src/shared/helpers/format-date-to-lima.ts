6053545bae7b68cc4d14054fd7e52ad19b08b2b9
backyta <kevi.baca18@gmail.com>
2025-06-01 15:58:28 -0500

    fix: :bug: fix format date

--- src/shared/helpers/format-date-to-lima.ts
+++ src/shared/helpers/format-date-to-lima.ts
@@ -1,8 +1,9 @@
 import { toZonedTime, format } from 'date-fns-tz';
 
 export const formatDateToLimaDayMonthYear = (dateString: Date): string => {
-  const timeZone = 'America/Lima';
   const date = new Date(dateString);
+  const isOnlyDate = typeof dateString === 'string' && /^\d{4}-\d{2}-\d{2}$/.test(dateString);
+  const timeZone = isOnlyDate ? 'UTC' : 'America/Lima';
 
   const zonedDate = toZonedTime(date, timeZone);
   const formattedDate = format(zonedDate, 'dd/MM/yyyy', { timeZone });
