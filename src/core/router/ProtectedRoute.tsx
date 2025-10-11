02624664bb6f3238579dcc2552b4521ca4348788
backyta <kevi.baca18@gmail.com>
2025-10-11 16:26:51 -0500

    fix: :bug: fix protected route validation

--- src/core/router/ProtectedRoute.tsx
+++ src/core/router/ProtectedRoute.tsx
@@ -4,8 +4,11 @@ import { LoadingSpinner } from '@/shared/components/spinners/LoadingSpinner';
 
 export const ProtectedRoute = () => {
   const authStatus = useAuthStore((state) => state.status);
+  const authToken = useAuthStore((state) => state.token);
+  const user = useAuthStore((state) => state.user);
 
-  if (authStatus === 'pending') return <LoadingSpinner />;
+  if (authToken && user && authStatus === 'pending') return <LoadingSpinner />;
+  if (!authToken && !user) return <Navigate to='/auth/login' replace />;
   if (authStatus === 'unauthorized') return <Navigate to='/auth/login' replace />;
 
   return <Outlet />;
