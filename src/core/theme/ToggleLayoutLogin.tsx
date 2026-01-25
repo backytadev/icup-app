54e00809b58ce69bd173d860746b80a0ddaadbd2
backyta <kevi.baca18@gmail.com>
2026-01-25 13:57:17 -0500

    fix: :bug: show toggle theme in movil page login

--- src/core/theme/ToggleLayoutLogin.tsx
+++ src/core/theme/ToggleLayoutLogin.tsx
@@ -3,9 +3,9 @@ import { ModeToggle } from '@/core/theme/mode-toggle';
 export const ToggleLayoutLogin = (): JSX.Element => {
   return (
     <div>
-  <div className='hidden md:block fixed right-6 top-4 z-50'>
-      <ModeToggle />
-    </div>
+      <div className='fixed right-6 top-4 z-50'>
+        <ModeToggle />
+      </div>
     </div>
   );
 };
